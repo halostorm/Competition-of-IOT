8662587f0f9a2a101888947b188526eb485e78f3
halostorm <2524416894@qq.com>
2017-12-21 18:05:57 +0800

    add detection and tracking

--- detect_in_camera/test/test.cpp
+++ detect_in_camera/test/test.cpp
@@ -143,9 +143,9 @@ int main(int argc, char **argv) {
 				right_detected = false;
 			}
 			if (!tracker_initialized && left_detected && right_detected) {//initializes the tracker//only do one time
-				target_left_box = ((double)target_left[0].x,(double)target_left[0].y,
+				target_left_box((double)target_left[0].x,(double)target_left[0].y,
 						(double)target_left[0].width, (double)target_left[0].height);
-				target_right_box = ((double)target_right[0].x,(double)target_right[0].y,
+				target_right_box((double)target_right[0].x,(double)target_right[0].y,
 						(double)target_right[0].width, (double)target_right[0].height);
 				if (!tracker_left->init(pic_left, target_left_box)) {
 					cout << "***Could not initialize left tracker...***\n";
